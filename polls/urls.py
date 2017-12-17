8194f1b73ea409469f56be30468e98ce3fc9db43
idforgitgmailcom <idforgit@gmail.com>
2017-12-17 21:30:31 +0900

    완료

--- polls/urls.py
+++ polls/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
-	url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
 	url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
+	url(r'^(?P<question_id>\d+)/result/$', views.result, name='result'),
 ]
