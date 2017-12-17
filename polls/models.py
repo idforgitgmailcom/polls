7f0f885848538076a41abda5d4e43c548e93dc64
idforgitgmailcom <idforgit@gmail.com>
2017-12-17 19:12:18 +0900

    Question&Choice 모델 작성 및 디비생성, 어드민에 등록

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+	text = models.CharField(max_length=200)
+	created_date = models.DateTimeField('created date')
+
+	def __str__(self):
+		return self.text
+
+class Choice(models.Model):
+	question = models.ForeignKey(Question)
+	text = models.CharField(max_length=200)
+	vote = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.text
